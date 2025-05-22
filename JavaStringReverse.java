264997855c92a614932c14b7ae752737bf32866e
amprajasri <104707068+amprajasri@users.noreply.github.com>
2025-05-22 17:50:03 +0530

    Create JavaStringReverse.java

---
+++ JavaStringReverse.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class JavaStringReverse {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        boolean isPalindrome=true;
+        for(int i=0, j=A.length()-1;i<A.length() && j>=0;i++,j--)
+        {
+           if(!(A.charAt(i)==A.charAt(j)))
+             {
+                isPalindrome=false;
+                break;
+             } 
+             
+        }
+        if(isPalindrome)
+             System.out.println("Yes");
+             else
+             System.out.println("No");
+        
+    }
+}
+
+
+
